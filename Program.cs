e02e011b22f7670a38cd7ac5ec767e9bae7f27f1
IlyaIIS <ilya1221221@mail.ru>
2020-11-03 23:56:36 +0500

    V3.0 Исправлен баг с неработающим кодом

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 
@@ -19,13 +18,24 @@ namespace FunctionBuilder
                 double xStart = Convert.ToDouble(input[2]);
                 double xEnd = Convert.ToDouble(input[3]);
 
-                string[] opn = ParseExpression(formula);
-
-                //foreach (string element in ParseExpression(formula)) Console.WriteLine(element);
-
-                File.WriteAllText("../../../output.txt", "gg");
+                WriteResult(formula, step, xStart, xEnd);
             }
         }
+        /////////////////////////////////////////////////////////КОНСТАНТЫ///////////////////////////////////////////////
+        static string[] signsList = { "+", "-", "*", "/", "^", "(", ")", "sin", "log", "!" };
+        static Dictionary<string, int> ordersList = new Dictionary<string, int>
+        {
+            {"(", 0},
+            {"+", 1},
+            {"-", 1},
+            {"*", 2},
+            {"/", 2},
+            {"^", 3},
+            {"sin", 10},
+            {"log", 10},
+            {"!", 10}
+        };
+        /////////////////////////////////////////////////////РАБОТА С ФАЙЛАМИ/////////////////////////////////////////////
         static string[] ReadFile()
         {
             string[] output = new string[4];
@@ -57,26 +67,12 @@ namespace FunctionBuilder
             }
         }
         //////////////////////////////////////////////////ОБРАТНАЯ ПОЛЬСКАЯ ЗАПИСТЬ//////////////////////////////////////
-
-        static string[] signsList = { "+", "-", "*", "/", "^", "(", ")", "sin", "log" };
-        static Dictionary<string, int> ordersList = new Dictionary<string, int>
-        {
-            {"(", 0},
-            {"+", 1},
-            {"-", 1},
-            {"*", 2},
-            {"/", 2},
-            {"^", 3},
-            {"sin", 10},
-            {"log", 10}
-        };
-        static string[] ParseExpression(string input)
+        static List<string> ParseExpression(string input)
         {
             string[] pInput = ParseInput(input);
             List<string> firstList = new List<string>();
             List<string> secondList = new List<string>();
-            string[] output;
-            bool isDebugEnabled = true;
+            bool isDebugEnabled = false;
 
             for (int i = 0; i < pInput.Length; i++)
             {
@@ -139,12 +135,7 @@ namespace FunctionBuilder
 
             for (int i = secondList.Count - 1; i >= 0; i--) firstList.Add(secondList[i]);
 
-            output = new string[firstList.Count];
-            for (int i = 0; i < output.Length; i++) output[i] = firstList[i];
-
-            foreach (string element in output) Console.WriteLine(element);
-
-            return output;
+            return firstList;
         }
 
         static string[] ParseInput(string input)
@@ -174,6 +165,14 @@ namespace FunctionBuilder
                 else
                 if (signsList.Contains(Convert.ToString(input[i])))
                 {
+                    if (input[i] == '-')
+                        if (i > 0)
+                        {
+                            if (!Char.IsDigit(input[i - 1]) && (input[i - 1] == '(' || input[i - 1] != ')'))
+                                preOutput.Add("0");
+                        }
+                        else
+                            preOutput.Add("0");
                     preOutput.Add(Convert.ToString(input[i]));
                 }
                 else
@@ -194,98 +193,125 @@ namespace FunctionBuilder
 
             return output;
         }
-
-        //Сама функция (вычисляет F(x))
-        static double GetY(double x)            
+        /////////////////////////////////////////////////////КАЛЬКУЛЯТОР//////////////////////////////////////////////////
+        static double Calculate(List<string> input)
         {
-            return Math.Round(Math.Sqrt(Math.Abs(x))*Math.Sin(x*(Math.Pow(-1,Math.Round(x))-1)))+x*(Math.Pow(-1,Math.Round(x))+1);
-        }
-
-        static double GetDouble(string str)
-        {
-            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru");
-            double output;
-            bool repeat;
-            int position = Console.CursorTop;
-
-            Console.Write(str);
-            do
+            for(int i = 0; i < input.Count; i++)
             {
-                repeat = !Double.TryParse(Console.ReadLine().Replace(".", ","), out output);
-                if (repeat)
+                if (signsList.Contains(input[i]))
                 {
-                    Console.SetCursorPosition(0, position);
-                    Console.Write("Введите корректное число!" + new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, position + 1);
-                    Console.ReadKey(true);
-                    Console.SetCursorPosition(0, position);
-                    Console.Write(str + new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(str.Length, position);
+                    switch (input[i])
+                    {
+                        case "+":
+                            input[i] = Convert.ToString(Convert.ToDouble(input[i - 2]) + Convert.ToDouble(input[i - 1]));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "-":
+                            input[i] = Convert.ToString(Convert.ToDouble(input[i - 2]) - Convert.ToDouble(input[i - 1]));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "*":
+                            input[i] = Convert.ToString(Convert.ToDouble(input[i - 2]) * Convert.ToDouble(input[i - 1]));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "/":
+                            input[i] = Convert.ToString(Convert.ToDouble(input[i - 2]) / Convert.ToDouble(input[i - 1]));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "^":
+                            input[i] = Convert.ToString(Math.Pow(Convert.ToDouble(input[i - 2]), Convert.ToDouble(input[i - 1])));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "sin":
+                            input[i] = Convert.ToString(Math.Sin(Convert.ToDouble(input[i - 1])));
+                            input.RemoveAt(i - 1);
+                            i -= 1;
+                            break;
+                        case "log":
+                            input[i] = Convert.ToString(Math.Log10(Convert.ToDouble(input[i - 2])) / Math.Log10((Convert.ToDouble(input[i - 1]))));
+                            input.RemoveAt(i - 1);
+                            input.RemoveAt(i - 2);
+                            i -= 2;
+                            break;
+                        case "!":
+                            double x = 1;
+                            for (int ii = 2; ii <= Convert.ToInt32(input[i - 1]); ii++) x *= ii;
+                            input[i] = Convert.ToString(x);
+                            input.RemoveAt(i - 1);
+                            i -= 1;
+                            break;
+                    }
                 }
-            } while (repeat);
+            }
 
-            return output;
+            return Convert.ToDouble(input[0]);
         }
-         
-        //Отрисовка результата
-        static void DrawResult(double step, double xStart, double xEnd)    
+
+        /////////////////////////////////////////////////////ВЫВОД РЕЗУЛЬТАТА//////////////////////////////////////////////////
+        static void WriteResult(string formula, double step, double xStart, double xEnd)    
         {
             double x, y;
-            int maxSize = GetMaxSizeXY(2, xStart, step, xEnd);
+            int maxSize = GetMaxSizeXY(formula, 2, xStart, step, xEnd);
+            string text = "";
 
             //Отрисовка
-            DrawBorder('─', '┌', '┬', '┐', maxSize);
+            text = WriteBorder('─', '┌', '┬', '┐', maxSize, text);
 
-            DrawMid(' ', '│', "X", "Y", (maxSize - 1) / 2, (maxSize - 1) / 2,
+            text = WriteMid(' ', '│', "X", "Y", (maxSize - 1) / 2, (maxSize - 1) / 2,
                                                             (int)Math.Ceiling((double)(maxSize - 1) / 2),
-                                                            (int)Math.Ceiling((double)(maxSize - 1) / 2));
+                                                            (int)Math.Ceiling((double)(maxSize - 1) / 2), text);
 
-            DrawBorder('─', '├', '┼', '┤', maxSize);
+            text = WriteBorder('─', '├', '┼', '┤', maxSize, text);
 
             x = xStart;
             do
             {
-                y = GetY(x);
+                y = GetY(formula, x);
 
-                DrawMid(' ', '│', Convert.ToString(x), Convert.ToString(y), 
-                 (maxSize - DigitSize(x)) / 2,
-                 (maxSize - DigitSize(y)) / 2,
-                 (int)Math.Ceiling((double)(maxSize - DigitSize(x)) / 2), 
-                 (int)Math.Ceiling((double)(maxSize - DigitSize(y)) / 2));
+                text = WriteMid(' ', '│', Convert.ToString(x), Convert.ToString(y), 
+                    (maxSize - DigitSize(x)) / 2,
+                    (maxSize - DigitSize(y)) / 2,
+                    (int)Math.Ceiling((double)(maxSize - DigitSize(x)) / 2), 
+                    (int)Math.Ceiling((double)(maxSize - DigitSize(y)) / 2), text);
 
                 x += step;
             } while ((step > 0 && x <= xEnd) || (step < 0 && x >= xEnd));
 
-            DrawBorder('─', '└', '┴', '┘', maxSize);
+            text = WriteBorder('─', '└', '┴', '┘', maxSize, text);
+
+            File.WriteAllText("../../../output.txt", text);
+        }
+        static double GetY(string formula, double x)
+        {
+            return Calculate(ParseExpression(formula.Replace("x", Convert.ToString(x))));
         }
 
-        static void DrawBorder(char char0, char char1, char char2, char char3, int maxSize)
+        static string WriteBorder(char char0, char char1, char char2, char char3, int maxSize, string text)
         {
-            Console.Write(char1);
-            Console.Write(new string(char0, maxSize));
-            Console.Write(char2);
-            Console.Write(new string(char0, maxSize));
-            Console.WriteLine(char3);
+            return text + char1 + new string(char0, maxSize) + char2 + new string(char0, maxSize) + char3 + "\n";
         }
-        static void DrawMid(char char0, char char1, string char2, string char3, int maxSize1, int maxSize2, int maxSize3, int maxSize4)
+        static string WriteMid(char char0, char char1, string char2, string char3, int maxSize1, int maxSize2, int maxSize3, int maxSize4, string text)
         {
-            Console.Write(char1);
-            DrawMark(char0, maxSize1);
-            Console.Write(char2);
-            DrawMark(char0, maxSize3);
-            Console.Write(char1);
-            DrawMark(char0, maxSize2);
-            Console.Write(char3);
-            DrawMark(char0, maxSize4);
-            Console.WriteLine(char1);
+            return text + char1 + new string(char0, maxSize1) + char2 + new string(char0, maxSize3) + char1 + 
+                new string(char0, maxSize2) + char3 + new string(char0, maxSize4) + char1 + "\n";
         }
 
-        static int GetMaxSizeXY(int maxSize, double x, double step, double xEnd)
+        static int GetMaxSizeXY(string formula, int maxSize, double x, double step, double xEnd)
         {
             double y;
             do
             {
-                y = GetY(x);
+                y = GetY(formula, x);
                 if (DigitSize(y) > maxSize) { maxSize = DigitSize(y); }
                 if (DigitSize(x) > maxSize) { maxSize = DigitSize(x); }
                 x += step;
@@ -297,14 +323,6 @@ namespace FunctionBuilder
         static int DigitSize(double d)            //Сколько символов в числе
         {
             return Convert.ToString(d).Length;
-        }
-
-        static void DrawMark(char mark, int n)    //Отрисовка знака mark n раз
-        {
-            for (int i = 1; i <= n; i++)
-            {
-                Console.Write(mark);
-            }
         }
     }
 }
